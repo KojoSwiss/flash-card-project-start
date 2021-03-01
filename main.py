b1b1704892255b873812e32b1ae8cd01e2378824
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-03-01 16:05:43 +0100

    UI completed with buttons

--- main.py
+++ main.py
@@ -10,11 +10,15 @@ front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=front_img)
 canvas.create_text(400, 150, text="French", fill="black", font=("Ariel", 40, "italic"))
 canvas.create_text(400, 263, text="Trouve", fill="black", font=("Ariel", 60, "bold"))
-canvas.grid(column=0, row=0)
+canvas.grid(column=0, row=0, columnspan=2)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 
 cross_img = PhotoImage(file="images/wrong.png")
-wrong = Button(image=cross_img)
+wrong = Button(image=cross_img, highlightthickness=0)
 wrong.grid(column=0, row=1)
+
+right_img = PhotoImage(file="images/right.png")
+right = Button(image=right_img, highlightthickness=0)
+right.grid(column=1, row=1)
 
 window.mainloop()
